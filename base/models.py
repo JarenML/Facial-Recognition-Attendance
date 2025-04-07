38a2f896d5961d7e1089d8f32efab63eae30a175
Jaren Ramos <jarenramos234@gmail.com>
2025-04-06 20:22:10 -0500

    Creando modelo Registrado y configurando ruta de medios

--- base/models.py
+++ base/models.py
@@ -13,7 +13,15 @@ class Detectado(models.Model):
     def __str__(self):
         return f'{self.nombre} - {self.fecha} {self.hora}'
     
+class Registrado(models.Model):
+    nombre = models.CharField(max_length=100)
+    informacion = models.TextField(null=True, blank=True)
+    imagen = models.ImageField(upload_to='registrados/', null=True, blank=True)
+    creado_en = models.DateTimeField(auto_now_add=True)
+    actualizado_en = models.DateField(auto_created=True)
 
+    def __str__(self):
+        return self.nombre
 
 
 
--- proyecto/settings.py
+++ proyecto/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,6 +128,11 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     "/var/www/static/",
 ]
+
+
+# RUTA DE MEDIOS
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
